fc538ae514114230648e47bd6b23f2af4d16b6a8
slowaudio <slowaudio@github.com>
2013-07-16 20:08:03 -0700

    fixed single digits and added itunes xml

---
+++ ex05.py
@@ -0,0 +1,18 @@
+name = 'Matthew R. Richardson'
+age = 34
+height = 76 #inches
+weight = 220 #lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+print "Let's talk about %s." % name
+print "He's %d inches tall." % height
+print "He's %d pounds heavy." % weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
+
+# this line is tricky, try to get it exactly right
+print "If I add %d, %d, and %d I get %d." % (
+    age, height, weight, age + height + weight)
--- ex13.py
+++ ex13.py
@@ -1,12 +1,15 @@
 # What's the difference between argv and raw_input()?
+#
 # The different has to do with where the user is required to give input. If 
 # they give your script inputs on the command line, then you use argv. If you 
 # want them to input using the keyboard while the script is running, then use 
 # raw_input().
-#
-# this is an import. add features from python feature set
+
+# import the sys module
 
 from sys import argv
+
+# unpack argv and assign it to all of the variables on the left in order
 
 script, first, second, third = argv
 
@@ -14,6 +17,3 @@ print "The script is called:", script
 print "Your first variable is:", first
 print "Your second variable is:", second
 print "Your third variable is:", third
-
-age = raw_input("How old are you? ")
-
