4c3c7b78a3cb7c3b33336157f45afe37481fb9dd
slowaudio <slowaudio@github.com>
2013-07-21 00:24:37 -0700

    ex15

--- ex15.py
+++ ex15.py
@@ -1,1 +1,39 @@
+# import argv (argument vector) from sys module
 
+from sys import argv
+
+# unpack argv - the argument we'll be expecting is a filename
+
+script, filename = argv
+
+# we might do this. we don't have to
+
+prompt = "> "
+
+# open the file and put its contents into txt
+
+txt = open(filename)
+
+# print a message with the filename
+
+print "Here's your file %r:" % filename
+
+# give the file txt a read command with the . and print the result
+
+print txt.read()
+
+## another print message
+#
+#print "Type the filename again:"
+#
+## ask for input with prompt. put result in file_again
+#
+file_again = raw_input(prompt)
+#
+## open the file and put its contents in txt_again
+#
+#txt_again = open(file_again)
+#
+## give the file txt_again a read command and print the result
+#
+#print txt_again.read()
